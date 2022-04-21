b2ea78d2c40c0f8321e9d038010e49ea5e0f2a3c
Esteferson Raphael <91302139+estraphael@users.noreply.github.com>
2022-04-21 00:25:40 -0300

    corrigi bug do button

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function login() {
 
-    //let divLoginResult = document.getElementById("loginResult");
-
     let loginUser = document.querySelector(".usuario").value;
 
-    let users = ["willian_birkin", "ada_wong", "albert_wesker"];
+    let users = ["william_birkin", "ada_wong", "albert_wesker"];
+
+    let resultado = document.getElementById("loginResult");
 
     for(i = 0; i < users.length; i++) {
         if(users.includes (loginUser)) {
-            alert("Welcome, " + loginUser);
+            resultado.innerHTML = "WELCOME, " + loginUser;
             break;
         } else {
-            alert("Access denied! Invalid username.")
+            resultado.innerHTML = "ACCESS DENIED! INVALID USERNAME."
             break;
         }
     }  
